a22789d8b38efd4fd987f34bda3cf51be0eba200
Joseph Holzman <josephholzman@macbookpro.mynetworksettings.com>
2025-09-12 03:04:31 -0400

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from '../components/hero/Hero';
 import Recentpress from '../components/recent-press/Recentpress'
-import Footer from '../components/footer/Footer'
-import Whoweare from '../components/who-we-are/Whoweare'
-import Product from '../components/product/Product'
 
 export default function Home() {
   return (
